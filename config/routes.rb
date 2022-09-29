953abed8879ab7be93cf6308f76aa8c422aa0815
richez003 <richemonday@gmail.com>
2022-09-29 06:36:51 -0500

    generated inventory root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'ineventory/index'
+  get '/inventories', to: 'inventory#index'
+  get 'inventories/:id', to: 'inventory#show', as: 'inventory'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
