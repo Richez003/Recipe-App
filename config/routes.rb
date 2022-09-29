6532c485a05e545e3c0335a67839e74283adefcb
richez003 <richemonday@gmail.com>
2022-09-29 10:09:28 -0500

    created controller and method for food and recipe

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,32 @@
 Rails.application.routes.draw do
-  get 'inventory_foods/index'
-  get 'inventory_foods/show'
+  get 'foods/index'
+  get 'foods/show'
+#   # get 'recipes/index'
+#   # get 'recipes/show'
+#   get 'inventory_foods/index'
+#   get 'inventory_foods/show'
 
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+#   devise_for :users
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "users#index"
-  resources :inventories, only: [:index, :show, :new, :create, :destroy]
+#   # Defines the root path route ("/")
+#   root "users#index"
+#   resources :inventories, only: [:index, :show, :new, :create, :destroy]
+#   resources :recipes, only: [:index, :show, :new, :create, :destroy]
+# end
+
+devise_for :users
+root "recipes#public"
+match 'public_recipes' => 'public_recipes#index', as: :public_recipes, via: :get
+
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+resources :users
+resources :recipes do
+  resources :recipe_foods
+end
+resources :foods, except: :update
+
+resources :inventories do
+  resources :inventory_foods
+end
 end
