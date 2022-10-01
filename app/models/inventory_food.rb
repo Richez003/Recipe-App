4f63b10d70e2424563a6bac120b3f49b4060e1f4
richez003 <richemonday@gmail.com>
2022-10-01 09:52:56 -0500

    fixed linters errors

--- app/models/inventory_food.rb
+++ app/models/inventory_food.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class InventoryFood < ApplicationRecord
   belongs_to :inventory, foreign_key: 'inventory_id'
   belongs_to :food, foreign_key: 'food_id'
--- spec/views/shopping_lists/index.html.erb_spec.rb
+++ spec/views/shopping_lists/index.html.erb_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
 RSpec.describe 'shopping_lists/index.html.erb', type: :view do
